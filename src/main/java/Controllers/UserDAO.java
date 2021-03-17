62b3571adb941df527c3e839fa87b6260695fd2a
LuLCotrim <lucaslcotrim@hotmail.com>
2021-03-17 15:10:31 -0300

    método de exclusão e edição de usuario

--- src/main/java/Controllers/UserDAO.java
+++ src/main/java/Controllers/UserDAO.java
@@ -24,7 +24,7 @@ public class UserDAO {
     private static final String path = System.getProperty("user.dir");
     private static final File config_file = new File(path + "/src/main/java/Utils/configuracaobd.properties");
     private static Connection connection = null;
-    
+
     ArrayList<User> list = new ArrayList<>();
 
     public ResultSet userAuth(User user) {
@@ -82,7 +82,7 @@ public class UserDAO {
         }
     }
 
-    public ArrayList<User> listUsers(){
+    public ArrayList<User> listUsers() {
         String sql = "select * from users";
         try {
             JDBCUtil.init(config_file);
@@ -92,7 +92,7 @@ public class UserDAO {
             PreparedStatement pstm = connection.prepareStatement(sql);
             ResultSet rs = pstm.executeQuery();
 
-            while(rs.next()){
+            while (rs.next()) {
                 User user = new User();
                 user.setUser_id(rs.getInt("user_id"));
                 user.setUser_login(rs.getString("user_login"));
@@ -100,10 +100,10 @@ public class UserDAO {
                 user.setUser_name(rs.getString("user_name"));
                 user.setUser_password(rs.getString("user_password"));
                 user.setUser_role(rs.getString("user_role"));
-                
+
                 list.add(user);
             }
-            
+
             return list;
 
         } catch (ClassNotFoundException erro) {
@@ -117,4 +117,54 @@ public class UserDAO {
             return null;
         }
     }
+
+    public void alterarUser(User user) {
+        String sql = "update users set user_name = ?, user_login = ?, user_password = ?, user_email = ?, user_role = ? where user_id = ?";
+        try {
+            JDBCUtil.init(config_file);
+            connection = JDBCUtil.getConnection();
+            connection.setAutoCommit(false);
+
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setString(1, user.getUser_name());
+            pstm.setString(2, user.getUser_login());
+            pstm.setString(3, user.getUser_password());
+            pstm.setString(4, user.getUser_mail());
+            pstm.setString(5, user.getUser_role());
+            pstm.setInt(6, user.getUser_id());
+
+            pstm.execute();
+            pstm.close();
+
+        } catch (ClassNotFoundException erro) {
+            System.out.println("Falha ao carregar o driver JDBC." + erro);
+        } catch (IOException erro) {
+            System.out.println("Falha ao carregar o arquivo de configuração." + erro);
+        } catch (SQLException erro) {
+            System.out.println("Falha na conexao, comando sql = " + erro);
+        }
+    }
+
+    public void deleteUser(User user) {
+        String sql = "delete from users where user_id = ?";
+        try {
+            JDBCUtil.init(config_file);
+            connection = JDBCUtil.getConnection();
+            connection.setAutoCommit(false);
+
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setInt(1, user.getUser_id());
+
+            pstm.execute();
+            pstm.close();
+
+        } catch (ClassNotFoundException erro) {
+            System.out.println("Falha ao carregar o driver JDBC." + erro);
+        } catch (IOException erro) {
+            System.out.println("Falha ao carregar o arquivo de configuração." + erro);
+        } catch (SQLException erro) {
+            System.out.println("Falha na conexao, comando sql = " + erro);
+        }
+    }
+
 }

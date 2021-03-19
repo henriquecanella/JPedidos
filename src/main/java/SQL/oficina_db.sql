395e10adb60cf78803567fe0ec8e573fa36dc4d5
João Vítor Retucci <jvitor.200003@gmail.com>
2021-03-19 08:52:01 -0300

    Added Inserts into .sql file

--- src/main/java/SQL/oficina_db.sql
+++ src/main/java/SQL/oficina_db.sql
@@ -45,3 +45,8 @@ CREATE table order_product(
     FOREIGN KEY(product_id) REFERENCES products(product_id)
 );
 
+INSERT INTO users (user_name, user_login, user_password, user_email, user_role) VALUES ("admin","admin","admin123","admin@email.com","admin");
+INSERT INTO users (user_name, user_login, user_password, user_email, user_role) VALUES ("gerente","gerente","gerente123","gerente@email.com","manager");
+INSERT INTO users (user_name, user_login, user_password, user_email, user_role) VALUES ("user","user","user123","user@email.com","employee");
+INSERT INTO orders (order_amount, order_customer_name, order_customer_phone, user_id) VALUES (1,"Cleyton", "(11)99999-9999",3);
+INSERT INTO products (product_name, product_description, product_price) VALUES ("Produto 1", "DescriÃ§Ã£o do produto 1", 200.99);
